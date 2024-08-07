cc510f37b2da93c0f229601e62a390a60e6e2f01
Ricardo <rick_ferry1@hotmail.com>
2024-08-07 14:20:23 -0300

    Adjusting

--- src/main/java/com/ekan/inss/config/SwaggerConfig.java
+++ src/main/java/com/ekan/inss/config/SwaggerConfig.java
@@ -15,7 +15,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.example.inss.controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.ekan.inss.controller"))
                 .paths(PathSelectors.any())
                 .build();
     }
